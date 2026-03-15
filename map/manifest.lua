7cc39311cf221413351bf11938c8ccba751f42e4
Sordal-GSIV <sordal@outlook.com>
2026-03-14 19:48:59 -0700

    feat(map): add package manifest

---
+++ map/manifest.lua
@@ -0,0 +1,7 @@
+return {
+    name = "map",
+    version = "1.0.0",
+    author = "Sordal",
+    description = "Interactive map viewer with room tracking and click-to-navigate",
+    depends = { "go2 >= 1.0" },
+}
